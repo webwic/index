cf7d4045aa9a1f0731a12cc9dc34869e1c3f2a2e
Mohammed Ziyad <ziyad@itproguide.com>
2022-02-03 16:26:18 +0530

    fixed scroll issues

---
+++ assets/js/fn.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         console.log("hi");
+//         if (scroll > 1400) {
+//           $(".c").css("color" , "black");
+//         }else if(scroll > 1601){
+//             $(".c").css("color" , "white");
+//         }  
+//         else{
+//             $(".c").css("color" , "white");  	
+//         }
+//     })
+//   })
+var top4 = $('#home').offset().top;
+var top5 = $('#about').offset().top;
+var top1 = $('#team').offset().top;
+var top2 = $('#port').offset().top;
+var top3 = $('#contact').offset().top;
+
+
+$(document).scroll(function() {
+  var scrollPos = $(document).scrollTop();
+  if(scrollPos >= top4 && scrollPos < top1){
+    $('.c').css('color', 'white');
+  
+}else if (scrollPos >= top1 && scrollPos < top2) {
+    $('.c').css('color', 'black');
+  } else if (scrollPos >= top2 && scrollPos < top3) {
+    $('.c').css('color', 'white');
+  } else if (scrollPos >= top3) {
+    $('.c').css('color', 'black');
+  }
+});
