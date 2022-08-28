edf849b332e1f63ff5791dc10a59c4a1698c4728
Niyas <Niyas@itproguide.com>
2022-08-28 11:02:38 +0400

    removed .env  file

---
+++ GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_92dcc45ee7c500cf\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_92dcc45ee7c500cf\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
