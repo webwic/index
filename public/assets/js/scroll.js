99d42f4d8c821e00ae57555fba14e30718bdc90c
ziyad188 <mohdziyadk@gmail.com>
2022-10-23 15:04:33 +0530

    changed nav bar

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -12,18 +12,31 @@ $(document).scroll(function () {
   var scrollPos = $(document).scrollTop();
   if (scrollPos >= top1 && scrollPos < top2) {
     $('.c').css('color', 'white');
+    $('#nav').css('background-color','transparent');
+
   } else if (scrollPos >= top2 && scrollPos < top3) {
     $('.c').css('color', 'black');
+    $('#nav').css('background-color','#dee2e6');
   }else if (scrollPos >= top3 && scrollPos < top4) {
-    $('.c').css('color', 'white');
+    $('.c').css('color', 'black');
+    $('#nav').css('background-color','#dee2e6');
+   // $('.c').css('color', 'white');
   } else if (scrollPos >= top4 && scrollPos < top5) {
     $('.c').css('color', 'black');
+    $('#nav').css('background-color','#dee2e6');
+   // $('.c').css('color', 'black');
   } else if (scrollPos >= top5 && scrollPos < top6) {
-    $('.c').css('color', 'white');
+    $('.c').css('color', 'black');
+    $('#nav').css('background-color','#dee2e6');
+    //$('.c').css('color', 'white');
   } else if (scrollPos >= top6) {
     $('.c').css('color', 'black');
+    $('#nav').css('background-color','#dee2e6');
+    //$('.c').css('color', 'black');
   }else{
     $('.c').css('color', 'white');
+    $('#nav').css('background-color','transparent');
+    // $('.c').css('color', 'white');
   }
 
 });
