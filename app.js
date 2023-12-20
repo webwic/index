bcfc36df83ea5edbe7965c7a8926e9d5384e213b
unknown <mohdziyadk@gmail.com>
2023-12-21 02:27:29 +0530

    added new port for webhook

--- app.js
+++ app.js
@@ -77,6 +77,42 @@ axios(config)
 app.post("/learn", function(req,res){
     res.sendFile(__dirname + "/learnmore.html");
 })
+app.get('/webhook', (req, res) => {
+  try {
+      console.log('GET: Someone is pinging me!');
+
+      let mode = req.query['hub.mode'];
+      let token = req.query['hub.verify_token'];
+      let challenge = req.query['hub.challenge'];
+
+      if (
+          mode &&
+          token &&
+          mode === 'subscribe' &&
+          process.env.Meta_WA_VerifyToken === token
+      ) {
+          return res.status(200).send(challenge);
+      } else {
+          return res.sendStatus(403);
+      }
+  } catch (error) {
+      console.error({ error });
+      return res.sendStatus(500);
+  }
+});
+
+app.post('/webhook', async (req, res) => {
+  try {
+      console.log('POST: Someone is pinging me!');
+      // Add your processing logic here
+
+      return res.sendStatus(200);
+  } catch (error) {
+      console.error({ error });
+      return res.sendStatus(500);
+  }
+});
+
 
 
 app.listen(process.env.PORT ||"3000", function(req,res){
